225f1585cc4f9d99f73807134635942f5c0b3341
BBA <stepmaniaredditrequests@gmail.com>
2026-03-26 19:02:54 -0400

    Update stadiums.js

--- stadiums.js
+++ stadiums.js
@@ -93,8 +93,8 @@ window.stadiumsDB = [
                 physics: { 
                     friction: 0.99, 
                     radiusMod: 0.9,
-                    basinSpinSpeed: 0.002, // Positive = Clockwise, Negative = Counter-Clockwise
-                    basinGrip: 0.05,        // How strongly the spinning floor pushes the Beyblade
+                    basinSpinSpeed: 0.015, // Positive = Clockwise, Negative = Counter-Clockwise
+                    basinGrip: 0.25,        // How strongly the spinning floor pushes the Beyblade
 					basinRadius: 0.3 // DEFAULT IS 0.3 (30%). Tweak this if your basin is wider!
                 },
                 ringOutZones: [
@@ -118,8 +118,8 @@ window.stadiumsDB = [
                 physics: { 
                     friction: 0.99, 
                     radiusMod: 0.9,
-                    basinSpinSpeed: 0.002, // Positive = Clockwise, Negative = Counter-Clockwise
-                    basinGrip: 0.05,        // How strongly the spinning floor pushes the Beyblade
+                    basinSpinSpeed: 0.015, // Positive = Clockwise, Negative = Counter-Clockwise
+                    basinGrip: 0.25,        // How strongly the spinning floor pushes the Beyblade
 					basinRadius: 0.3 // DEFAULT IS 0.3 (30%). Tweak this if your basin is wider!
                 },
                 ringOutZones: [
