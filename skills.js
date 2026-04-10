7c16ce4ac3b48eac9d38d3bbbe6bed59c357ba80
BBA <stepmaniaredditrequests@gmail.com>
2026-04-10 13:24:25 -0400

    Update skills.js

--- skills.js
+++ skills.js
@@ -995,7 +995,7 @@ window.SkillEngine = {
                     
                     // The circle is 3.5x the beyblade's radius
                     state.circleRadius = (bey.baseRadius || bey.radius) * 3.5;
-					let forwardPush = = state.circleRadius + 40;
+					let forwardPush = state.circleRadius + 40;
                     
                     // Place the center of the circle OUT IN FRONT of the beyblade
                     state.csCenterX = bey.x + (state.csDirX * forwardPush);
